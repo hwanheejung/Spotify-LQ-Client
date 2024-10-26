fea8a888773c1624bc937dfd70ef4cc27ee731fb
hwanheejung <hwanheejung117@gmail.com>
2024-10-27 05:14:54 +0900

    chore: set tailwind config theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,41 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+    colors: {
+      gray: {
+        0: '#ffffff',
+        100: '#b3b3b3',
+        300: '#535353',
+        500: '#212121',
+        700: '#121212',
+        900: '#000000',
       },
+      spotifyGreen: '#1db954',
+      transparent: 'transparent',
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+    },
+    fontSize: {
+      xxs: '10px',
+      xs: '12px',
+      sm: '16px',
+      md: '18px',
+      lg: '20px',
+      xl: '24px',
+      '2xl': '28px',
+      '3xl': '32px',
+      '4xl': '36px',
+    },
+    lineHeights: {
+      xxs: '12px',
+      xs: '16px',
+      sm: '24px',
+      md: '32px',
+      lg: '40px',
+      xl: '48px',
+      '2xl': '52px',
+    },
+    extend: {
       fontFamily: {
         gotham: [
           'var(--font-gotham-book)',
@@ -19,6 +49,12 @@ const config: Config = {
           'var(--font-gotham-light)',
           'var(--font-gotham-medium)',
         ],
+      },
+      height: {
+        dvh: 'var(--dynamic-vh)',
+      },
+      minHeight: {
+        dvh: 'var(--dynamic-vh)',
       },
     },
   },
