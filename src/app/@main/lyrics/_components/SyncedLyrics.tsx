fddd555764866d3c1cfe95f25f17163c25705d8b
hwanheejung <hwanheejung117@gmail.com>
2024-12-02 16:38:43 +0900

    feat: add auto scroll to the current lyrics line

--- src/app/@main/lyrics/_components/SyncedLyrics.tsx
+++ src/app/@main/lyrics/_components/SyncedLyrics.tsx
@@ -1,4 +1,5 @@
 import { usePlaybackStore } from '@/lib/stores/playback.store'
+import { useEffect, useRef } from 'react'
 
 interface SyncedLine {
   time: number // ms
@@ -7,6 +8,8 @@ interface SyncedLine {
 
 const SyncedLyrics = ({ lyrics }: { lyrics: string }) => {
   const currentTime = usePlaybackStore((state) => state.currentTrack?.position)
+  const lineRefs = useRef<(HTMLParagraphElement | null)[]>([])
+
   const parsedLyrics: SyncedLine[] = lyrics.split('\n').map((line: string) => {
     const [time, text] = line.split(']')
 
@@ -23,12 +26,32 @@ const SyncedLyrics = ({ lyrics }: { lyrics: string }) => {
     }
   })
 
+  useEffect(() => {
+    if (!currentTime) return
+
+    const currentLineIndex = parsedLyrics.findIndex(
+      (line, index) =>
+        currentTime >= line.time &&
+        (index === parsedLyrics.length - 1 ||
+          currentTime < parsedLyrics[index + 1].time),
+    )
+
+    if (currentLineIndex !== -1 && lineRefs.current[currentLineIndex])
+      lineRefs.current[currentLineIndex]?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      })
+  }, [currentTime, parsedLyrics])
+
   return (
     <div className="h-full overflow-y-scroll px-10 py-20 scrollbar-hide">
       <div className="whitespace-pre-line text-xl font-bold text-gray-100 filter">
         {parsedLyrics.map((line, index) => (
           <p
             key={index}
+            ref={(el) => {
+              lineRefs.current[index] = el
+            }}
             className={`${
               currentTime! >= line.time &&
               (index === parsedLyrics.length - 1 ||
