29a13547921519068b598aaca0b1635e67a28e63
hwanheejung <hwanheejung117@gmail.com>
2024-12-10 00:24:33 +0900

    feat: implement New Releases using GridList

--- src/app/@main/page.tsx
+++ src/app/@main/page.tsx
@@ -1,7 +1,17 @@
-'use client'
+import { PreloadQuery } from '@/lib/graphql/apollo-client'
+import { GET_NEW_RELEASES } from '@/lib/queries/albums.query'
+import { Suspense } from 'react'
+import NewReleases from './_components/NewReleases'
+import NewReleasesSkeleton from './_components/NewReleases.skeleton'
 
 export default function Home() {
   return (
-    <div className="h-full overflow-y-scroll rounded-lg bg-gray-700 scrollbar-hide" />
+    <div className="h-full overflow-y-scroll rounded-lg bg-gray-700 scrollbar-hide">
+      <PreloadQuery query={GET_NEW_RELEASES} variables={{ limit: 5 }}>
+        <Suspense fallback={<NewReleasesSkeleton />}>
+          <NewReleases />
+        </Suspense>
+      </PreloadQuery>
+    </div>
   )
 }
